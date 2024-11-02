a21d4d1ac80ba5f710d8c783b665df827c3f6dcc
GoldDeniel <aranyadhun@gmail.com>
2024-11-02 18:09:08 +0100

    :rocket: Web API with MongoDB

--- Program.cs
+++ Program.cs
@@ -1,17 +1,26 @@
 using TodoApi.Models;
 using Microsoft.EntityFrameworkCore;
-
+using Restapi_Pluszpont.Models;
+using Restapi_Pluszpont.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(
+            options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 
 builder.Services.AddDbContext<TodoContext>(opt =>
     opt.UseInMemoryDatabase("TodoList"));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<BookStoreDatabaseSettings>(
+    builder.Configuration.GetSection("BookStoreDatabase"));
+builder.Services.AddSingleton<BooksService>();
+
+
 
 var app = builder.Build();
 
